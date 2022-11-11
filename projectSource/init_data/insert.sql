1e4208b35665ec0f11faad4fe4f85c0b4d04d865
StevenD6378 <stda6378@colorado.edu>
2022-11-11 12:54:03 -0700

    Added Demo Login2

--- projectSource/init_data/insert.sql
+++ projectSource/init_data/insert.sql
@@ -1,7 +1,6 @@
 INSERT INTO users (username, email, steam_id, password) VALUES
 ('abc', 'abc@gmail.com', '76561197960435530', '123'),
-('aaa', 'aaa@gmail.com', '76561197960435530', '111'),
-('bbb', 'aaa@gmail.com', '76561197977933423', '111');
+('aaa', 'aaa@gmail.com', '76561197960435530', '111');
 
 COPY games
 FROM '/data/steam.csv'
